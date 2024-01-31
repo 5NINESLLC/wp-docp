e3a65d38f4be12793a8f8c572b54fdc3280c7ae1
axjacobs <jacobs7@wisc.edu>
2024-01-30 18:23:14 -0600

    Add various phi scrubbing endpoints

--- types/data_access/class-docc-data-access.php
+++ types/data_access/class-docc-data-access.php
@@ -51,9 +51,54 @@ class Data_Access extends Docc_Controller
 
     public function wp_ajax_scrub_phi_data()
     {
-        echo $this->get_user_phi_entries();
+        $phi_entries = $this->get_user_phi_entries();
+
+        $this->scrub_user_phi_data($phi_entries);
 
         wp_die();
+    }
+
+    private function scrub_user_phi_data($entries)
+    {
+        foreach ($entries as $entry) {
+            $phi_entry = $this->check_for_phi($entry);
+
+            if ($phi_entry['contains_phi']) $this->scrub_phi_entry($entry, $phi_entry);
+        }
+
+        return $entries;
+    }
+
+    private function scrub_phi_entry($entry, $phi_entry)
+    {
+        foreach ($phi_entry as $field => $phi) {
+            if ($field === 'contains_phi') continue;
+            foreach ($phi[1] as $entity) {
+                if ($entity->Type === 'PHONE_OR_FAX') $entity->Type = 'PHONE';
+                $entry[$field] = str_replace($entity->Text, "[" . $entity->Type . "]", $entry[$field]);
+            }
+        }
+
+        GFAPI::update_entry($entry);
+
+        return $entry;
+    }
+
+    private function exec_phi_scrubber($entry_text)
+    {
+        if (empty($entry_text) || !$entry_text) return [];
+
+        $entry_text = escapeshellarg($entry_text);
+
+        $command = "ssh -i ~/TonyDesktop.pem ec2-user@44.202.217.41 aws comprehendmedical detect-phi --text \'{$entry_text}\'";
+
+        exec("$command", $output);
+        
+        $jsonString = implode("", $output);
+
+        $jsonObject = json_decode($jsonString);
+
+        return $jsonObject;
     }
 
     public function docc_delete_old_data()
@@ -75,6 +120,19 @@ class Data_Access extends Docc_Controller
             $entries = GFAPI::get_entries($form['id'], $search_criteria);
 
             foreach ($entries as $entry) GFAPI::delete_entry($entry['id']);
+        }
+
+        wp_die();
+    }
+
+    public function docc_phi_scrubber()
+    {
+        $entries = $this->get_daily_phi_entries();
+
+        foreach ($entries as $entry) {
+            $phi_entry = $this->check_for_phi($entry);
+
+            if ($phi_entry['contains_phi']) $this->scrub_phi_entry($entry, $phi_entry);
         }
 
         wp_die();
@@ -105,40 +163,77 @@ class Data_Access extends Docc_Controller
         wp_die();
     }
 
-    private function contains_phi($value) {
-        return true;
+    private function check_for_phi($entry) {
+        $phi_fields = [11, 13, 15];
+
+        $entry_phi['contains_phi'] = false;
+
+        foreach ($phi_fields as $field) $entry_phi[$field] = $this->check_phi_fields($entry[$field]);
+
+        foreach ($entry_phi as $phi) if (isset($phi[0]) && $phi[0]) $entry_phi['contains_phi'] = true;
+
+        return $entry_phi;
+    }
+
+    private function check_phi_fields($entry_text)
+    {
+        $scrubber_result = $this->exec_phi_scrubber($entry_text);
+
+        if (isset($scrubber_result->Entities) && !empty($scrubber_result->Entities)) return [true, $scrubber_result->Entities];
+        
+        return [false, []];
     }
 
     private function get_user_phi_entries()
     {
         $user_id = get_current_user_id();
         $search_criteria = ['status' => 'active', 'field_filters' => [['key' => 'created_by', 'value' => $user_id]]];
-        $paging = array('offset' => 0, 'page_size' => 100);
         $form = $this->GetGravityFormByTitle("New Observation");
         $entries = GFAPI::get_entries($form['id'], $search_criteria);
 
         $phi_fields = [];
 
         foreach ($entries as $entry) {
-            if ($this->contains_phi($entry)) {
-                $phi_fields[] = $entry;
-                /*[
-                    'entry_id' => $entry['id'],
-                    3 => $entry[3],
-                    11 => $entry[11],
-                    13 => $entry[13],
-                    15 => $entry[15]
-                ];*/
+            $phi_entry = $this->check_for_phi($entry);
+
+            if ($phi_entry['contains_phi']) $phi_fields[] = $entry;
+        }
+
+        return $phi_fields;
+    }
+
+    private function get_daily_phi_entries()
+    {
+        $search_criteria = ['status' => 'active', 'field_filters' => [['key' => 'date_created', 'value' => date('Y-m-d', strtotime('-1 day')), 'operator' => '>=']]];
+        $form = $this->GetGravityFormByTitle("New Observation");
+        $entries = GFAPI::get_entries($form['id'], $search_criteria);
+
+        return $entries;
+    }
+
+    private function highlight_phi($entry, $phi_entry)
+    {
+        foreach ($phi_entry as $field => $phi) {
+            if ($field === 'contains_phi') continue;
+            foreach ($phi[1] as $entity) {
+                $entry[$field] = str_replace($entity->Text, "<span class='phi-highlight'>" . $entity->Text . "</span>", $entry[$field]);
             }
         }
 
-        return json_encode($phi_fields);
+        return $entry;
     }
 
     public function observations_with_phi($atts)
     {
         $form = $this->GetGravityFormByTitle("New Observation");
         $entries = $this->get_user_phi_entries();
+
+        foreach ($entries as &$entry) {
+            $phi_entry = $this->check_for_phi($entry);
+
+            if ($phi_entry['contains_phi']) $entry = $this->highlight_phi($entry, $phi_entry);
+        }
+
         return $this->DisplayObservations($form, $entries);
     }
 
@@ -156,5 +251,14 @@ class Data_Access extends Docc_Controller
             return $date_utc->format('m/d/Y');
         }
         return $date;
+    }
+
+    public function gform_after_submission($entry, $form)
+    {
+        if ($form['title'] !== "New Observation") return;
+
+        $phi_entry = $this->check_for_phi($entry);
+
+        if ($phi_entry['contains_phi']) $this->scrub_phi_entry($entry, $phi_entry);
     }
 }
